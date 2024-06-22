2679f7b543dc687cec82fcdf0fca9b1749aae3a6
Sidorela <sidorelahamzallari063@gmail.com>
2024-06-22 21:23:10 +0200

    modified:   Rental/settings.py

--- Rental/settings.py
+++ Rental/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ze*9*ty#^2n9b+7kjswryunw(@=fft^4pxsok0k-i7b8it%nl8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['easy-auto-rental.onrender.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
